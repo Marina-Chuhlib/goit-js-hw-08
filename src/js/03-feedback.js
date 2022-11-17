c2444b1930805b4458992d5c3dd3fd673d6fc710
Marina-Chuhlib <marinachuhlib@gmail.com>
2022-11-17 23:21:59 +0300

    Done task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+
+const formData = {};
+
+const onFormInput = e => {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+const onFormSub = e => {
+  e.preventDefault();
+
+  const mail = formRef.elements.email.value;
+  const msg = formRef.elements.message.value;
+
+  e.target.reset();
+  localStorage.removeItem('feedback-form-state');
+
+  console.log(`email : ${mail}`);
+  console.log(`message : ${msg}`);
+};
+
+const textareaInput = () => {
+  const getSaveValue = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  try {
+    return (
+      ((formRef.email.value = getSaveValue.email) &&
+        (formRef.message.value = getSaveValue.message || '')) ||
+      []
+    );
+  } catch (error) {
+    return [];
+  }
+};
+
+textareaInput();
+
+formRef.addEventListener('input', throttle(onFormInput, 500));
+formRef.addEventListener('submit', onFormSub);
