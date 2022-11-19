c0a1b4a0da45604be466ea1d95d2402d02a66799
Marina-Chuhlib <marinachuhlib@gmail.com>
2022-11-19 10:58:10 +0300

    Correction

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,43 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-console.log(galleryItems);
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+console.log(galleryItems);
+
 const galleryMarkup = ({ original, preview, description }) => {
-  return `<li>
-    <a class="gallery__item" href= '${original}' "large-image.jpg">
+  return `<li><a class="gallery__item" href= '${original}' "large-image.jpg">
     <img class="gallery__image" src= '${preview}' "small-image.jpg" alt= '${description}'"Image description" />
-  </a></li>
-      `;
+  </a></li>`;
 };
 
 const refs = {
   gallery: document.querySelector('.gallery'),
 };
 
-const render = () => {
-  const pictures = galleryItems.map(galleryMarkup).join('');
-  refs.gallery.insertAdjacentHTML('beforeend', pictures);
-};
+const pictures = galleryItems.map(galleryMarkup).join('');
+refs.gallery.insertAdjacentHTML('beforeend', pictures);
 
-const onPictureClick = event => {
-  event.preventDefault();
-  if (!event.target.classList.contains('gallery__image')) return;
-};
-
-render();
-refs.gallery.addEventListener('click', onPictureClick);
-
-const gallery = new SimpleLightbox('.gallery a', {
+new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionPosition: 'bottom',
   captionDelay: 250,
-});
-
-gallery.on('show.simplelightbox', function () {
-  return gallery;
 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,7 +4,7 @@ const formRef = document.querySelector('.feedback-form');
 
 const STORAGE_KEY_FORM = 'feedback-form-state';
 
-const formData = {};
+let formData = {};
 
 const onFormInput = e => {
   formData[e.target.name] = e.target.value;
@@ -12,34 +12,30 @@ const onFormInput = e => {
   localStorage.setItem(STORAGE_KEY_FORM, JSON.stringify(formData));
 };
 
+const getValueLocalStorage = () => {
+  const getSaveValue = JSON.parse(localStorage.getItem(STORAGE_KEY_FORM));
+
+  if (localStorage.getItem(STORAGE_KEY_FORM)) {
+    formData.email = getSaveValue.email;
+    formData.message = getSaveValue.message || '';
+
+    formRef.email.value = getSaveValue.email;
+    formRef.message.value = getSaveValue.message || '';
+  }
+};
+
+getValueLocalStorage();
+
 const onFormSub = e => {
   e.preventDefault();
 
-  const mail = formRef.elements.email.value;
-  const msg = formRef.elements.message.value;
+  console.log(formData);
 
   e.target.reset();
   localStorage.removeItem(STORAGE_KEY_FORM);
 
-  console.log(`email : ${mail}`);
-  console.log(`message : ${msg}`);
+  formData = {};
 };
-
-const textareaInput = () => {
-  const getSaveValue = JSON.parse(localStorage.getItem(STORAGE_KEY_FORM));
-
-  try {
-    return (
-      ((formRef.email.value = getSaveValue.email) &&
-        (formRef.message.value = getSaveValue.message || '')) ||
-      []
-    );
-  } catch (error) {
-    return [];
-  }
-};
-
-textareaInput();
 
 formRef.addEventListener('input', throttle(onFormInput, 500));
 formRef.addEventListener('submit', onFormSub);
